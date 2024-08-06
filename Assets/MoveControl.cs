e200829850bc76564aceafe3c45d99f2f35a52e9
Joao Franco <joao53963676@edu.pa.senac.br>
2024-08-06 17:37:31 -0300

    controlador adicionado 6/8
    adicionei script controlador

---
+++ Assets/MoveControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveControl : MonoBehaviour
+{
+
+    [SerializeField] Vector3 _move;
+    [SerializeField] Rigidbody2D _rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        _rb = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _rb.velocity = new Vector2(_move.x, _rb.velocity.y);
+
+    }
+
+
+
+
+
+    public void SetMove(InputAction.CallbackContext value)
+    {
+
+        _move = value.ReadValue<Vector3>();
+
+    }
+}
