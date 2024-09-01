1c92f98d6652b25066be96e9f3fede111e288faa
c4rool <mapiguari53@gmail.com>
2024-08-31 21:41:49 -0300

    animação

---
+++ Assets/ivy/movimentação/personagem.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+   { [SerializeField] Vector3 _move;
+    [SerializeField] Rigidbody2D _rb;
+    bool _facingRight;
+    [SerializeField] float _SPEED;
+
+    //pulo plataforma
+    bool _checkgroud;
+    [SerializeField] float _forcejump;
+    public Animator anim ;
+    void Start()
+    {
+
+
+    }
+
+
+  void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+           anim.SetBool("personagem",true);
+        }
+
+        _rb.velocity = new Vector2(_move.x * _SPEED, _rb.velocity.y);
+
+        if (_move.x > 0 && _facingRight == true)
+        {
+            flip();
+
+        }
+        else if (_move.x < 0 && _facingRight == false)
+        {
+            flip();
+        }
+    }
+    public void SetMove(InputAction.CallbackContext value)
+
+    {
+        _move = value.ReadValue<Vector3>();
+
+    }
+
+    //pulo plataforma
+    public void SetJump(InputAction.CallbackContext value)
+
+    {
+        if (_checkgroud == true)
+        {
+            _rb.AddForce(Vector2.up * _forcejump, ForceMode2D.Impulse);
+
+        }
+
+
+    }
+
+    void flip()
+
+    {
+        _facingRight = !_facingRight;
+        float x = transform.localScale.x;
+        x *= -1;
+        transform.localScale = new Vector2(x, transform.localScale.y);
+    }
+    //pulo plataforma
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+            Debug.Log("tocou no chao");
+
+        _checkgroud = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+            Debug.Log("saiu no chao");
+
+        _checkgroud = false;
+    }
+}
